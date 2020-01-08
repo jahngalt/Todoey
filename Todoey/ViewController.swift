017c1b1f06b8529fd2850dd2ab906841d0646c2a
Oleg Kudimov <ovkudimov@gmail.com>
2020-01-08 17:32:34 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Oleg Kudimov on 1/8/20.
+//  Copyright © 2020 Oleg Kudimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
